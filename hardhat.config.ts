c9b66159890033860233f4c89f61bfb59713a139
Sabir Aboobaker <sabir.aboobaker@accubits.com>
2022-08-30 13:35:49 +0530

    updated configs

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,5 @@
-
-import { HardhatUserConfig } from 'hardhat/config';
-import '@nomiclabs/hardhat-etherscan';
-import '@nomiclabs/hardhat-waffle';
-import '@openzeppelin/hardhat-upgrades';
-import 'hardhat-contract-sizer';
-import '@typechain/hardhat';
-import 'hardhat-gas-reporter';
-import 'solidity-coverage';
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
